ee44f9b12f5b6e8ed71a15c047cb91431fb66b63
DARAND\Andrea <andrea18.edu@gmail.com>
2023-07-11 21:27:36 -0400

    segunda version

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
-import { Button, CircularProgress, Paper, Typography } from '@mui/material';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import {
+  Breadcrumbs,
+  Button,
+  CircularProgress,
+  Paper,
+  Typography,
+  Link,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 // const useStyles = makeStyles((theme) => ({
 //   container: {
@@ -20,37 +27,33 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
   // const classes = useStyles();
-  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [selectedQuestion, setSelectedQuestion] = useState(1);
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchQuestion({ id: 2 })
+    setLoading(true);
+    fetchQuestion({ id: selectedQuestion })
       .then((data) => {
-        setSelectedQuestion(data);
-        setLoading(false);
+        setCurrentQuestion(data);
         console.log(data);
       })
       .catch((error) => {
-        console.log('Error al obtener la pregunta:', error);
-      });
-  }, []);
+        console.log("Error al obtener la pregunta:", error);
+      })
+      .finally(() => setLoading(false));
+  }, [selectedQuestion]);
 
   const fetchQuestion = async (params) => {
     try {
-      const response = await axios.post('http://localhost:8000/questions', params);
+      const response = await axios.post(
+        "http://localhost:8000/questions",
+        params
+      );
       return response.data;
     } catch (error) {
       throw error;
-    }
-  };
-
-  const handleOptionClick = async (option) => {
-    try {
-      const response = await fetchQuestion({ id: option });
-      setSelectedQuestion(response);
-      console.log('Respuesta seleccionada:', response);
-    } catch (error) {
-      console.log('Error al obtener la pregunta:', error);
     }
   };
 
@@ -58,14 +61,37 @@ const App = () => {
     <div>
       {loading ? (
         <CircularProgress />
-      ) : selectedQuestion ? (
+      ) : currentQuestion ? (
         <Paper>
-          <Typography variant="h6">Pregunta {selectedQuestion.id}</Typography>
-          <Typography variant="body1">{selectedQuestion.message}</Typography>
-          <Button variant="contained" color="primary" onClick={() => handleOptionClick(selectedQuestion.oui)}>
+          <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="#">
+              Home
+            </Link>
+            <Link underline="hover" color="inherit" href="#">
+              Catalog
+            </Link>
+            <Link underline="hover" color="inherit" href="#">
+              Accessories
+            </Link>
+            <Link underline="hover" color="inherit" href="#">
+              New Collection
+            </Link>
+            <Typography color="text.primary">Belts</Typography>
+          </Breadcrumbs>
+          <Typography variant="h6">Pregunta {currentQuestion.id}</Typography>
+          <Typography variant="body1">{currentQuestion.message}</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setSelectedQuestion(currentQuestion.oui)}
+          >
             Oui
           </Button>
-          <Button variant="contained" color="secondary" onClick={() => handleOptionClick(selectedQuestion.non)}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => setSelectedQuestion(currentQuestion.non)}
+          >
             Non
           </Button>
         </Paper>
